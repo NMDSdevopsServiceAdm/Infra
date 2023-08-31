cd55cd6de83aa8b8403c677924d2aa3154e67bc3
jaseera@madetech.com <jaseera@madetech.com>
2023-08-31 15:03:07 +0100

    Refactor terraform into multiple tr files

---
+++ terraform/modules/pipeline/iam.tf
@@ -0,0 +1,68 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com","codepipeline.amazonaws.com"]
+    }
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::914197850242:role/CodeBuildServiceRole"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name               = "CodeBuildServiceRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+
+data "aws_iam_policy_document" "codebuildservicerole_policy" {
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObjectVersion",
+      "s3:GetObject",
+      "s3:GetBucketLocation",
+      "s3:GetBucketAcl",
+      "s3:*",
+      "logs:PutLogEvents",
+      "logs:CreateLogStream",
+      "logs:CreateLogGroup",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "codestar-connections:*",
+      "codepipeline:*",
+      "codecommit:GitPull",
+      "codebuild:*"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:*",
+    ]
+
+    resources = [aws_iam_role.codebuild_role.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "codebuildservicerole_policy" {
+  name   = "codebuildservicerole_policy"
+  role   = aws_iam_role.codebuild_role.id
+  policy = data.aws_iam_policy_document.codebuildservicerole_policy.json
+}

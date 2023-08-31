61f43b88b860f568f03760e9422ce17fcebe441b
jaseera@madetech.com <jaseera@madetech.com>
2023-08-31 16:36:36 +0100

    Add pipeline for main branch commits

--- terraform/modules/pipeline/codepipeline.tf
+++ terraform/modules/pipeline/codepipeline.tf
@@ -3,8 +3,8 @@ resource "aws_codestarconnections_connection" "codestar_github" {
   provider_type = "GitHub"
 }
 
-resource "aws_codepipeline" "codepipeline" {
-  name     = "tf-test-pipeline"
+resource "aws_codepipeline" "codepipeline_feature_branch" {
+  name     = "tf-feature-branch-pipeline"
   role_arn = aws_iam_role.codebuild_role.arn
 
   artifact_store {
@@ -52,27 +52,57 @@ resource "aws_codepipeline" "codepipeline" {
       role_arn = aws_iam_role.codebuild_role.arn
     }
   }
+}
 
-  # stage {
-  #   name = "Deploy"
+resource "aws_codepipeline" "codepipeline_main_branch" {
+  name     = "tf-main-branch-pipeline"
+  role_arn = aws_iam_role.codebuild_role.arn
 
-  #   action {
-  #     name            = "Deploy"
-  #     category        = "Deploy"
-  #     owner           = "AWS"
-  #     provider        = "CloudFormation"
-  #     input_artifacts = ["build_output"]
-  #     version         = "1"
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
 
-  #     configuration = {
-  #       ActionMode     = "REPLACE_ON_FAILURE"
-  #       Capabilities   = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
-  #       OutputFileName = "CreateStackOutput.json"
-  #       StackName      = "MyStack"
-  #       TemplatePath   = "build_output::sam-templated.yaml"
-  #     }
-  #   }
-  # }
+    # encryption_key {
+    #   id   = data.aws_kms_alias.s3kmskey.arn
+    #   type = "KMS"
+    # }
+  }
+
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+      configuration = {
+        ConnectionArn    = aws_codestarconnections_connection.codestar_github.arn
+        FullRepositoryId = "NMDSdevopsServiceAdm/Infra"
+        BranchName       = "main"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = "test"
+      }
+      role_arn = aws_iam_role.codebuild_role.arn
+    }
+  }
 }
 
 

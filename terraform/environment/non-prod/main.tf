fa4eb0020039966706e2d7dc4707b4bcfe827fc2
Sam Tripp <1519984+sbt92@users.noreply.github.com>
2023-08-31 11:01:48 +0100

    Comment out modules were not using for pipeline work

--- terraform/environment/non-prod/main.tf
+++ terraform/environment/non-prod/main.tf
@@ -7,6 +7,8 @@ terraform {
     bucket = "terraform-state-sfc"
     key    = "state/terraform.tfstate"
     region = "eu-west-1"
+    access_key = "<access-key>"
+    secret_key = "<secret-key>"
   }
 }
 # module "frontend" {
@@ -15,19 +17,19 @@ terraform {
 #   environment = var.environment
 # }
 
-module "backend" {
-  source = "../../modules/backend"
+# module "backend" {
+#   source = "../../modules/backend"
 
-  environment = var.environment
-  app_runner_cpu = var.app_runner_cpu
-  app_runner_memory = var.app_runner_memory
-  private_subnet_ids = module.networking.private_subnets
-  security_group_ids = module.networking.security_group_id
-  }
+#   environment = var.environment
+#   app_runner_cpu = var.app_runner_cpu
+#   app_runner_memory = var.app_runner_memory
+#   private_subnet_ids = module.networking.private_subnets
+#   security_group_ids = module.networking.security_group_id
+#   }
 
-module "networking" {
-  source = "../../modules/networking"
-}
+# module "networking" {
+#   source = "../../modules/networking"
+# }
 
 module "pipeline" {
   source = "../../modules/pipeline"

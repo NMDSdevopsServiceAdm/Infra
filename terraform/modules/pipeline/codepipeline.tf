55278139c5fea6301d87efe2df4417a9b535ac39
jaseera@madetech.com <jaseera@madetech.com>
2023-09-04 15:51:07 +0100

    Remove feature pipeline

--- terraform/modules/pipeline/codepipeline.tf
+++ terraform/modules/pipeline/codepipeline.tf
@@ -3,60 +3,8 @@ resource "aws_codestarconnections_connection" "codestar_github" {
   provider_type = "GitHub"
 }
 
-resource "aws_codepipeline" "codepipeline_feature_branch" {
-  name     = "tf-feature-branch-pipeline"
-  role_arn = aws_iam_role.codebuild_role.arn
-
-  artifact_store {
-    location = aws_s3_bucket.codepipeline_bucket.bucket
-    type     = "S3"
-
-    # encryption_key {
-    #   id   = data.aws_kms_alias.s3kmskey.arn
-    #   type = "KMS"
-    # }
-  }
-
-  stage {
-    name = "Source"
-    action {
-      name             = "Source"
-      category         = "Source"
-      owner            = "AWS"
-      provider         = "CodeStarSourceConnection"
-      version          = "1"
-      output_artifacts = ["source_output"]
-      configuration = {
-        ConnectionArn    = aws_codestarconnections_connection.codestar_github.arn
-        FullRepositoryId = "NMDSdevopsServiceAdm/Infra"
-        BranchName       = "feat/terraform-pipeline"
-        DetectChanges = "false"
-      }
-    }
-  }
-
-  stage {
-    name = "Build"
-
-    action {
-      name             = "Build"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      input_artifacts  = ["source_output"]
-      # output_artifacts = ["build_output"]
-      version          = "1"
-
-      configuration = {
-        ProjectName = "feature"
-      }
-      role_arn = aws_iam_role.codebuild_role.arn
-    }
-  }
-}
-
 resource "aws_codebuild_webhook" "codepipeline_feature_branch_webhook" {
-  project_name = "feature"
+  project_name = "test"
   build_type   = "BUILD"
   filter_group {
     filter {

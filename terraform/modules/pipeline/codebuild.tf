9653f0358f185cf4feb755c8f0e80495d6d2938c
jaseera@madetech.com <jaseera@madetech.com>
2023-09-05 10:49:02 +0100

    Rename cloudwatch group name
    Co-authored-by: Sam Tripp <sbt92@users.noreply.github.com>

--- terraform/modules/pipeline/codebuild.tf
+++ terraform/modules/pipeline/codebuild.tf
@@ -2,7 +2,7 @@ resource "aws_codebuild_project" "codebuild_feature" {
   name          = "feature"
   description   = "terraform pipeline feature branch"
   build_timeout = "5"
-  service_role  = aws_iam_role.codebuild_role.arn
+  service_role  = aws_iam_role.codebuild_role.arn 
 
   artifacts {
     type = "NO_ARTIFACTS"
@@ -17,14 +17,13 @@ resource "aws_codebuild_project" "codebuild_feature" {
 
   logs_config {
     cloudwatch_logs {
-      group_name  = "log-group"
-      stream_name = "log-stream"
+      group_name  = "/aws/codebuild/${aws_codebuild_project.codebuild_feature.name}"
     }
   }
 
   source {
     type            = "GITHUB"
-    location        = "https://github.com/NMDSdevopsServiceAdm/Infra.git"
+    location        = "https://github.com/NMDSdevopsServiceAdm/Infra.git" 
     git_clone_depth = 1
     buildspec = "buildspec/feature.yml"
 
@@ -53,14 +52,13 @@ resource "aws_codebuild_project" "codebuild_main" {
 
   logs_config {
     cloudwatch_logs {
-      group_name  = "log-group"
-      stream_name = "log-stream"
+      group_name  = "/aws/codebuild/${aws_codebuild_project.codebuild_main.name}"
     }
   }
 
   source {
     type            = "GITHUB"
-    location        = "https://github.com/NMDSdevopsServiceAdm/Infra.git"
+    location        = "https://github.com/NMDSdevopsServiceAdm/Infra.git" 
     git_clone_depth = 1
     buildspec = "buildspec/main.yml"
 

1eab2045d7b544479f947a97260c97ff54422ed2
jaseera@madetech.com <jaseera@madetech.com>
2023-09-21 16:12:23 +0100

    Add terraform infra for staging environment

---
+++ terraform/environment/staging/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-sfc-staging"
+    key    = "state/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+module "frontend" {
+  source = "../../modules/frontend"
+
+  environment = var.environment
+}
+
+module "backend" {
+  source = "../../modules/backend"
+
+  environment = var.environment
+  app_runner_cpu = var.app_runner_cpu
+  app_runner_memory = var.app_runner_memory
+  private_subnet_ids = module.networking.private_subnets
+  security_group_ids = module.networking.security_group_id
+  }
+
+module "networking" {
+  source = "../../modules/networking"
+}
+
+# module "pipeline" {
+#   source = "../../modules/pipeline"
+
+#   environment = var.environment
+# }

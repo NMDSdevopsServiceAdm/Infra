4f87588996e9ded62965291fe20af339310ce575
jaseera@madetech.com <jaseera@madetech.com>
2023-10-02 14:29:35 +0100

    Set correct image url

--- terraform/modules/backend/app_runner.tf
+++ terraform/modules/backend/app_runner.tf
@@ -20,7 +20,7 @@ resource "aws_apprunner_service" "sfc_app_runner" {
       access_role_arn = aws_iam_role.app_runner_erc_access_role.arn
     }
     image_repository {
-      image_identifier      = "${aws_ecr_repository.sfc_backend_ecr_repository.repository_url}:latest"
+      image_identifier      = "636146736465.dkr.ecr.eu-west-1.amazonaws.com/sfc-backend-build-images:latest"
       image_repository_type = "ECR"
       image_configuration {
         port = 3000

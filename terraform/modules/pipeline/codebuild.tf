6b7ec309f3edb1f1f6c92792abdbced421c605aa
Sam Tripp <1519984+sbt92@users.noreply.github.com>
2023-10-04 14:13:36 +0100

    Add deploy to staging codebuild project
    Co-authored-by: Jaseera Abubacker <jpmaHub@users.noreply.github.com>

--- terraform/modules/pipeline/codebuild.tf
+++ terraform/modules/pipeline/codebuild.tf
@@ -284,3 +284,38 @@ resource "aws_codebuild_project" "codebuild_asc_wds_build_deploy_backend" {
     }
   }
 }
+
+resource "aws_codebuild_project" "codebuild_asc_wds_deploy_staging" {
+  name          = "asc-wds-deploy-staging"
+  description   = "deploy the asc-wds application to the staging environment"
+  build_timeout = "5"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_MEDIUM"
+    image                       = "aws/codebuild/standard:7.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "/aws/codebuild/asc-wds-deploy/staging"
+    }
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/NMDSdevopsServiceAdm/SFC-Migration-Test.git" 
+    git_clone_depth = 1
+    buildspec = "buildspec/deploy-staging.yml"
+
+    git_submodules_config {
+      fetch_submodules = true
+    }
+  }
+}
--- terraform/modules/pipeline/iam.tf
+++ terraform/modules/pipeline/iam.tf
@@ -139,6 +139,7 @@ data "aws_iam_policy_document" "codebuildservicerole_policy" {
       aws_codebuild_project.codebuild_asc_wds_build_test_performance.arn,
       aws_codebuild_project.codebuild_asc_wds_build_deploy_frontend.arn,
       aws_codebuild_project.codebuild_asc_wds_build_deploy_backend.arn,
+      aws_codebuild_project.codebuild_asc_wds_deploy_staging.arn
       ]
   }
 

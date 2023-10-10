dba21c1057401564df0e76c8278a4f7d439cb52f
Sam Tripp <1519984+sbt92@users.noreply.github.com>
2023-10-10 11:26:10 +0100

    Add deploy staging pipeline stage; refactor old build pipeline step

--- terraform/modules/pipeline/codebuild.tf
+++ terraform/modules/pipeline/codebuild.tf
@@ -68,6 +68,41 @@ resource "aws_codebuild_project" "codebuild_terraform_apply" {
   }
 }
 
+resource "aws_codebuild_project" "codebuild_terraform_apply_staging" {
+  name          = "asc-wds-infra-terraform-apply-staging"
+  description   = "terraform apply the main branch to the AWS staging account"
+  build_timeout = "5"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:7.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "/aws/codebuild/terraform-apply-staging"
+    }
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/NMDSdevopsServiceAdm/Infra.git" 
+    git_clone_depth = 1
+    buildspec = "buildspec/terraform-apply-staging.yml"
+
+    git_submodules_config {
+      fetch_submodules = true
+    }
+  }
+}
+
 resource "aws_codebuild_project" "codebuild_asc_wds_build" {
   name          = "asc-wds-build"
   description   = "build the asc-wds application"
--- terraform/modules/pipeline/iam.tf
+++ terraform/modules/pipeline/iam.tf
@@ -135,7 +135,8 @@ data "aws_iam_policy_document" "codebuildservicerole_policy" {
     ]
     resources = [
       aws_codebuild_project.codebuild_terraform_validate.arn, 
-      aws_codebuild_project.codebuild_terraform_apply.arn, 
+      aws_codebuild_project.codebuild_terraform_apply.arn,
+      aws_codebuild_project.codebuild_terraform_apply_staging.arn, 
       aws_codebuild_project.codebuild_asc_wds_build.arn,
       aws_codebuild_project.codebuild_asc_wds_build_test_frontend.arn,
       aws_codebuild_project.codebuild_asc_wds_build_test_backend.arn,

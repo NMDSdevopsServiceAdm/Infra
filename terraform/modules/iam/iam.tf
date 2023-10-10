96c12ea727cf09e3a3156eaf9a0ea3976fd35464
jaseera@madetech.com <jaseera@madetech.com>
2023-10-10 16:26:14 +0100

    Add codebuild terraform state bucket policy

--- terraform/modules/iam/iam.tf
+++ terraform/modules/iam/iam.tf
@@ -35,3 +35,28 @@ resource "aws_iam_role_policy_attachment" "codebuild_cross_account_access_servic
   policy_arn = aws_iam_policy.codebuild_cross_account_access_service_policy.arn
 }
 
+
+data "aws_iam_policy_document" "codebuild_terraform_state_bucket_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::terraform-state-sfc-staging"]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    resources = ["arn:aws:s3:::terraform-state-sfc-staging/state/terraform.tfstate"]
+  }
+}
+
+resource "aws_iam_policy" "codebuild_terraform_state_bucket_policy" {
+  name        = "CodebuildTerraformStateBucketPolicy"
+  description = "To provide s3 terraform staging buckek permission to codebuild build and deploy"
+  policy      = data.aws_iam_policy_document.codebuild_terraform_state_bucket_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_codebuild_terraform_state_bucket_policy" {
+  role       = aws_iam_role.codebuild_cross_account_access_service_role.name
+  policy_arn = aws_iam_policy.codebuild_terraform_state_bucket_policy.arn
+}

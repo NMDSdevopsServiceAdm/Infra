28779f9683e8e9f7fb0d7dbfbe18d2385ec9ee75
Zuhal-Ayob <90700942+Zuhal-Ayob@users.noreply.github.com>
2023-10-16 10:13:27 +0100

    Add prod environment (#17)
    * Add prod environment
    
    * Grant prod account  ecr access
    
    * Add buildspec , codebuild project and pipeline for production

--- terraform/modules/pipeline/codepipeline.tf
+++ terraform/modules/pipeline/codepipeline.tf
@@ -94,6 +94,24 @@ resource "aws_codepipeline" "codepipeline_main_branch" {
       role_arn = aws_iam_role.codebuild_role.arn
     }
   }
+   stage {
+    name = "DeployProductionInfrastructure"
+
+    action {
+      name             = "DeployProductionInfrastructure"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["terraform_production_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.codebuild_terraform_apply_production.name
+      }
+      role_arn = aws_iam_role.codebuild_role.arn
+    }
+  }
 }
 
 resource "aws_codepipeline" "codepipeline_asc_wds_build" {

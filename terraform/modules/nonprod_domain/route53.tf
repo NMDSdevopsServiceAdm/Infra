4f2b9511f29bc787fe166e9182e74f796e51455d
Sam Tripp <1519984+sbt92@users.noreply.github.com>
2023-11-29 12:14:36 +0000

    WIP

--- terraform/modules/nonprod_domain/route53.tf
+++ terraform/modules/nonprod_domain/route53.tf
@@ -17,3 +17,31 @@ resource "aws_route53domains_registered_domain" "ascwds_co_uk" {
     name = "ns-484.awsdns-60.com"
   }
 }
+
+resource "aws_route53_zone" "ascwds_co_uk_zone" {
+  name = aws_route53domains_registered_domain.ascwds_co_uk.id
+}
+
+resource "aws_route53_record" "staging_ascwds_co_uk" {
+  zone_id = aws_route53_zone.ascwds_co_uk_zone.zone_id
+  name    = "staging"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["d100lf5hrbnpno.cloudfront.net"]
+}
+
+# resource "aws_route53_record" "preprod_ascwds_co_uk" {
+#   zone_id = aws_route53_zone.ascwds_co_uk_zone.zone_id
+#   name    = "preprod.${aws_route53domains_registered_domain.ascwds_co_uk.id}"
+#   type    = "CNAME"
+#   ttl     = 300
+#   records = [aws_eip.lb.public_ip]
+# }
+
+# resource "aws_route53_record" "benchmarks_ascwds_co_uk" {
+#   zone_id = aws_route53_zone.ascwds_co_uk_zone.zone_id
+#   name    = "preprod.${aws_route53domains_registered_domain.ascwds_co_uk.id}"
+#   type    = "CNAME"
+#   ttl     = 300
+#   records = [aws_eip.lb.public_ip]
+# }

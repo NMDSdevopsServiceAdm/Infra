979a97e8823890501d2785363a62446cc25c7cf5
Sam Tripp <1519984+sbt92@users.noreply.github.com>
2023-12-15 09:56:12 +0000

    Add vars for  EC2 Instances

--- terraform/modules/sfc_reports_jobs/ec2_instance.tf
+++ terraform/modules/sfc_reports_jobs/ec2_instance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "sfc_reports" {
   ami           = "ami-0694d931cee176e7d"
-  instance_type = "t2.micro"
+  instance_type = var.sfc_reports_instance_type
   key_name  = aws_key_pair.sfc_reports_ssh_key.key_name
   subnet_id =  var.public_subnet_ids[0]
-  security_groups  = [var.security_group_ids[0], aws_security_group.ssh_access.id]
+  vpc_security_group_ids  = [var.security_group_ids[0], aws_security_group.ssh_access.id]
 
  tags = {
     Name = "sfc-reports"
@@ -12,7 +12,7 @@ resource "aws_instance" "sfc_reports" {
 
 resource "aws_key_pair" "sfc_reports_ssh_key" {
   key_name   = "sfc-reports-ssh-key"
-  public_key = ""
+  public_key = aws_ssm_parameter.sfc_reports_ssh_public_key.value
 }
 
 resource "aws_security_group" "ssh_access" {
@@ -22,6 +22,6 @@ resource "aws_security_group" "ssh_access" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [""]
+    cidr_blocks = []
   }
 }

0d03baed5c73d32bd7c3a93d7f4a7cfb018f4011
Sam Tripp <1519984+sbt92@users.noreply.github.com>
2023-12-15 16:15:20 +0000

    Decommission SFC Reports Lambdas

--- terraform/modules/sfc_reports_jobs/variables.tf
+++ terraform/modules/sfc_reports_jobs/variables.tf
@@ -3,31 +3,6 @@ variable "environment" {
   type        = string
 }
 
-variable "database_password" {
-  description = "The password for the RDS database"
-  type        = string
-}
-
-variable "database_username" {
-  description = "The username for the RDS database"
-  type        = string
-}
-
-variable "database_host" {
-  description = "Name of the host for the RDS database"
-  type        = string
-}
-
-variable "database_port" {
-  description = "The post for the RDS database"
-  type        = number
-}
-
-variable "database_name" {
-  description = "Name of the RDS database"
-  type        = string
-}
-
 variable "private_subnet_ids" {
   description = "The ID's of the private subnet used by the App Runner VPC connector"
 }

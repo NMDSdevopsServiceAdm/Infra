0041f9f9bf5e9381c53d1147961a5f110d96bff2
Sam Tripp <1519984+sbt92@users.noreply.github.com>
2024-01-02 16:33:29 +0000

    Add /api into staging

--- terraform/environment/staging/variable.tf
+++ terraform/environment/staging/variable.tf
@@ -42,6 +42,10 @@ variable "app_runner_max_container_instances_size" {
   type        = number
 }
 
+variable "app_runner_url" {
+  description = "The app runner url"
+  type        = string
+}
 variable "app_runner_max_concurrency" {
   description = "The maximum number of concurrency for a single AWS App runner container instance"
   type        = number

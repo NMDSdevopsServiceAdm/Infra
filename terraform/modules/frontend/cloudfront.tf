f479dd2c0eb392d8840a5fd8e9c17bfb5d4fc47e
Zuhal-Ayob <zuhal.ayob@skillsforcare.org.uk>
2024-01-08 12:23:19 +0000

    terraform fmt
    Co-authored-by: Sam Tripp <sbt92@users.noreply.github.com>

--- terraform/modules/frontend/cloudfront.tf
+++ terraform/modules/frontend/cloudfront.tf
@@ -1,8 +1,8 @@
 resource "aws_cloudfront_distribution" "sfc_frontend_distribution" {
   origin {
-    domain_name              = aws_s3_bucket_website_configuration.sfc_frontend_bucket_website_configuration.website_endpoint
-    origin_id                = aws_s3_bucket_website_configuration.sfc_frontend_bucket_website_configuration.website_endpoint
-  custom_origin_config {
+    domain_name = aws_s3_bucket_website_configuration.sfc_frontend_bucket_website_configuration.website_endpoint
+    origin_id   = aws_s3_bucket_website_configuration.sfc_frontend_bucket_website_configuration.website_endpoint
+    custom_origin_config {
       http_port              = "80"
       https_port             = "443"
       origin_protocol_policy = "http-only"
@@ -11,9 +11,9 @@ resource "aws_cloudfront_distribution" "sfc_frontend_distribution" {
   }
 
   origin {
-    domain_name              = var.app_runner_url
-    origin_id                = var.app_runner_url
-  custom_origin_config {
+    domain_name = var.app_runner_url
+    origin_id   = var.app_runner_url
+    custom_origin_config {
       http_port              = "80"
       https_port             = "443"
       origin_protocol_policy = "https-only"
@@ -22,21 +22,21 @@ resource "aws_cloudfront_distribution" "sfc_frontend_distribution" {
   }
 
   origin {
-    domain_name              = aws_s3_bucket_website_configuration.sfc_public_bucket_website_configuration.website_endpoint
-    origin_id                = aws_s3_bucket_website_configuration.sfc_public_bucket_website_configuration.website_endpoint
-  custom_origin_config {
+    domain_name = aws_s3_bucket_website_configuration.sfc_public_bucket_website_configuration.website_endpoint
+    origin_id   = aws_s3_bucket_website_configuration.sfc_public_bucket_website_configuration.website_endpoint
+    custom_origin_config {
       http_port              = "80"
       https_port             = "443"
       origin_protocol_policy = "http-only"
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
-  
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "Couldfront distribution for ${var.environment}"
-  aliases = [var.domain_name]
+
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "Couldfront distribution for ${var.environment}"
+  aliases         = [var.domain_name]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -51,13 +51,13 @@ resource "aws_cloudfront_distribution" "sfc_frontend_distribution" {
       }
     }
 
-    viewer_protocol_policy =  "redirect-to-https"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
   }
 
-    ordered_cache_behavior {
+  ordered_cache_behavior {
     path_pattern     = "api/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
@@ -79,7 +79,7 @@ resource "aws_cloudfront_distribution" "sfc_frontend_distribution" {
     viewer_protocol_policy = "redirect-to-https"
   }
 
-      ordered_cache_behavior {
+  ordered_cache_behavior {
     path_pattern     = "public/*"
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
@@ -104,9 +104,9 @@ resource "aws_cloudfront_distribution" "sfc_frontend_distribution" {
 
   custom_error_response {
     error_caching_min_ttl = 10
-    error_code = 404
-    response_code = 404
-    response_page_path = "/index.html"
+    error_code            = 404
+    response_code         = 404
+    response_page_path    = "/index.html"
   }
 
   restrictions {
@@ -118,7 +118,7 @@ resource "aws_cloudfront_distribution" "sfc_frontend_distribution" {
 
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.certificate.arn
-    ssl_support_method = "sni-only"
-    
+    ssl_support_method  = "sni-only"
+
   }
 }
